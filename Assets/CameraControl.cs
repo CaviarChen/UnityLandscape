c5fd28f86f043d4577da5c435879ea24c09991b0
CaviarChen <yzj@vlabpro.com>
2017-08-13 15:59:46 +1000

    Add Camera Control

---
+++ Assets/CameraControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class CameraControl : MonoBehaviour {
+
+	public float mouseSensitivitiy = 5.0f;
+	public float moveSpeed = 4.0f;
+
+	// Use this for initialization
+	void Start () {
+		Cursor.visible = false;
+		Cursor.lockState = CursorLockMode.Locked;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		// mouse
+		float yaw = mouseSensitivitiy * Input.GetAxis("Mouse X");
+		float pitch = -mouseSensitivitiy * Input.GetAxis("Mouse Y");
+		transform.eulerAngles += new Vector3(pitch, yaw, 0.0f);
+
+
+		if (Input.GetKey (KeyCode.W)) {
+			transform.position += transform.forward * moveSpeed * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			transform.position -= transform.forward * moveSpeed * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			transform.position -= transform.right * moveSpeed * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.position += transform.right * moveSpeed * Time.deltaTime;
+		}
+
+		
+	}
+}
