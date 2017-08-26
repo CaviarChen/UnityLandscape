08b22c3816c417a8a441bcebe1779e79b761c332
dodojk <tangzhe@TangMBP.local>
2017-08-26 16:18:58 +1000

    Add sun and moon

---
+++ Assets/Sun.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sun : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround (Vector3.zero, Vector3.right, 10f * Time.deltaTime);
+		transform.LookAt (Vector3.zero);
+	}
+}
